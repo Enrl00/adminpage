fe41e041d24e42b9554862a6cd36d116acb0f512
enerel <enerelts0011@gmail.com>
2022-09-20 12:38:21 +0800

    sep 20 classwork

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,107 +1,95 @@
 import * as React from 'react';
 import { Box } from '@mui/system';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper
+} from '@mui/material';
 import AccordionContainer from './accordionContainer';
-import { useState } from 'react';
-import { useOrderContext } from '../provider/OrderProvider';
-
-export const CollapsibleTable=(text)=> {
-  const [packedOrder, setPackedOrder] = useState([])
-  const [deliveredOrder, setDelilveredOrder] = useState([])
-  const [mistakenOrder, setMistakenOrder] = useState([])
-  const{orderInfo} = useOrderContext()
-  let packedArr=[];
-  let deliveredArr=[];
-  let mistakenArr=[];
-  text.data.order.map((el) => {
-    if(el.status === 'Packed'){
-      packedArr.push(el);
-      setPackedOrder(packedArr)
-  } if(el.status === 'Delivered'){
-    deliveredArr.push(el);
-      setDelilveredOrder(deliveredArr)
-  } if(el.status === 'Mistaken'){
-    mistakenArr.push(el);
-      setMistakenOrder(mistakenArr)
+const styles ={
+  tableBody:{
+    width:'300px',
+    height:'540px',
+    overflow: 'scroll',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center' 
+  },
+  table:{
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
   }
-  })
+}
+
+export const CollapsibleTable = ({ data }) => {
+  let orderedArr = data.filter(({ status }) => status === "Ordered");
+  let packedArr = data.filter(({ status }) => status === "Packed");
+  let deliveredArr = data.filter(({ status }) => status === "Delivered");
+  let mistakenArr = data.filter(({ status }) => status === "Mistaken");
   return (
     <Box>
-      <TableContainer component={Paper}>
-      <Table sx={{
-        display:'flex',
-        flexDirection:'column',
-        alignItems:'center'
-      }}>
-        <TableHead>
-          <TableRow>
-            <TableCell />
-            <TableCell>{text.value}</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody style={{height:'40vh', overflow:'scroll', display:'flex',flexDirection:'column', width:'90%'}}>
-          <AccordionContainer data={orderInfo} />
-        </TableBody>
-      </Table>
-    </TableContainer>
-    <TableContainer component={Paper} id='packed'>
-      <Table sx={{
-        display:'flex',
-        flexDirection:'column',
-        alignItems:'center'
-      }}>
-        <TableHead>
-          <TableRow>
-            <TableCell />
-            <TableCell>{text.value}</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody style={{height:'40vh', overflow:'scroll', display:'flex',flexDirection:'column', width:'90%'}}>
-          <AccordionContainer data={packedOrder} />
-        </TableBody>
-      </Table>
-    </TableContainer>
-    <TableContainer component={Paper} id='delivered'>
-      <Table sx={{
-        display:'flex',
-        flexDirection:'column',
-        alignItems:'center'
-      }}>
-        <TableHead>
-          <TableRow>
-            <TableCell />
-            <TableCell>{text.value}</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody style={{height:'40vh', overflow:'scroll', display:'flex',flexDirection:'column', width:'90%'}}>
-          <AccordionContainer data={deliveredOrder} />
-        </TableBody>
-      </Table>
-    </TableContainer>
-    <TableContainer component={Paper} id='mistaken'>
-      <Table sx={{
-        display:'flex',
-        flexDirection:'column',
-        alignItems:'center'
-      }}>
-        <TableHead>
-          <TableRow>
-            <TableCell />
-            <TableCell>{text.value}</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody style={{height:'40vh', overflow:'scroll', display:'flex',flexDirection:'column', width:'90%'}}>
-          <AccordionContainer data={mistakenOrder} />
-        </TableBody>
-      </Table>
-    </TableContainer>
+      <TableContainer component={Paper} >
+        <Table sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}>
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell>Ordered</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody style={{backgroundColor: '#F5F5F7'}} sx={styles.tableBody}>
+            {orderedArr.length !== 0 && <AccordionContainer data={orderedArr} />}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TableContainer component={Paper} id='packed'>
+        <Table sx={styles.table }>
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell>Packed</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody style={{ backgroundColor: '#FFF9F1'}}  sx={styles.tableBody}>
+            {packedArr.length !== 0 && <AccordionContainer data={packedArr} />}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TableContainer component={Paper} id='delivered'>
+        <Table sx={styles.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell>Delivered</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody style={{ backgroundColor: '#FAFFF5' }}  sx={styles.tableBody}>
+            <AccordionContainer data={deliveredArr} />
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TableContainer component={Paper} id='mistaken'>
+        <Table sx={styles.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell>Mistaken</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody style={{ backgroundColor: '#FAFFF5' }}  sx={styles.tableBody}>
+            <AccordionContainer data={mistakenArr}/>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
-    
+
   );
 }
